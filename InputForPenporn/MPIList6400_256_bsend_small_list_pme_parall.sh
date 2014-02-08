a439df8f28ed0f15f6b9ba34a7094473671d2a5c
ondemerdash <ondemerdash@berkeley.edu>
2014-02-07 16:09:00 -0800

    Added .gitignore

---
+++ InputForPenporn/MPIList6400_256_bsend_small_list_pme_parall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#$ -S /bin/bash
+#$ -N MPI6400
+#$ -q all.q
+#$ -pe orte 256 
+#$ -cwd
+
+module load ompi-intel 
+mpirun /home/ondemerdash/GoodVersionsNoEwaldThgLoop/DontUseReplica_UseImagesNoCutOrigLoopNoFuncNoEwald3bNo1bodyOptEwaldPermOMPParallelTiming/MPIVersionNewest3List_CobarNonDiscrete8/OptSmallList/FewerArrayAlloc/ParallelizeAll/analyze_parallel_bcast_evenload_bsend_pme_parall.x -k /home/ondemerdash/Xyz_and_Keyfiles/6400_cluster_no_openmp.key /home/ondemerdash/Xyz_and_Keyfiles/6400_cluster.xyz E > ParAllPMEFewerArrayAllocBsendOptSmallListlog6400_DontUseReplica_UseImagesNoCutOrigLoopNoFuncNoEwald3bNo1bodyOptEwaldPermOMPParallelTiming_MPIVersionNewest3List_CobarNonDiscrete8_256thread 
